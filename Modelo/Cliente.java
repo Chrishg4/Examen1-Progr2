895f620057599f3fdb8142111f6674b21cd7c5d9
Chrishg4 <chhernandezga@est.utn.ac.cr>
2024-10-22 09:34:40 -0600

    Se gurada la informcion

--- Modelo/Cliente.java
+++ Modelo/Cliente.java
@@ -9,5 +9,41 @@ package Modelo;
  * @author Student
  */
 public class Cliente {
+    private int id;
+    private String nombre;
+    private String telefono;
+  
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public Cliente(int id, String nombre, String telefono) {
+        this.id = id;
+        this.nombre = nombre;
+        this.telefono = telefono;
+    }
+
+    public Cliente() {
+        this(0,"","");
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "id=" + id + ", nombre=" + nombre + ", telefono=" + telefono + '}';
+    }
+    
+    
     
 }
